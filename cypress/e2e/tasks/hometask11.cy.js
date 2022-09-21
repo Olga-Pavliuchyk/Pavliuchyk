87fb2b326dd4bf9f8729ee46696d69db401ef877
Olga-Pavliuchyk <olga.pavliuchyk@gmail.com>
2022-09-21 23:47:02 +0300

    hometask11

--- cypress/e2e/tasks/hometask11.cy.js
+++ cypress/e2e/tasks/hometask11.cy.js
@@ -16,6 +16,7 @@ describe ('Authorization' , () => {
       }); 
 });
 
+
 // Напишите тест на форму на отправку комментария
 // https://yevheniiahlovatska.editorx.io/web-practice/post/what-to-wear-to-a-blacktie-event
 
@@ -44,6 +45,7 @@ describe ('Добавление комментария' , () => {
     cy.contains('[data-hook="submit-button"]', 'Delete').click();
   });
 });
+
 
 describe ('Добавление комментария' , () => {
  
