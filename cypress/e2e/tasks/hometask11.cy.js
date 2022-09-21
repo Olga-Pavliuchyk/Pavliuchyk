fbc81c18a0e49d3fd332463bdcc97091687633f5
Olga-Pavliuchyk <olga.pavliuchyk@gmail.com>
2022-09-21 23:42:25 +0300

    hometask11

---
+++ cypress/e2e/tasks/hometask11.cy.js
@@ -0,0 +1,68 @@
+// Напишите тест на авторизацию https://yevheniiahlovatska.editorx.io/web-practice/
+
+describe ('Authorization' , () => {   
+  it('', () => { 
+      cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/');
+      cy.wait(7000); // иначе не открывалось окно login (при неявном ожидании тоже)
+      cy.get('[data-testid="section-container"] button').click(); // Log in
+      cy.wait(4000);
+      cy.get('button[data-testid*="SignUp"]').click(); // Log in
+      cy.get('[data-testid*="Email"] button', {timeout : 5000}).click(); // Log in with Email
+      cy.get('input[id*="emailInput"]').type('fortestonly@mail.com', {delay : 20}); 
+      cy.get('input[id*="passwordInput"]').type('Qwerty123*', {delay : 20});
+      cy.get('[id*="okButton"] button').click();
+      cy.get('[aria-label~="account"]').should('exist').should('be.visible');
+      cy.get('[aria-label~="account"] div').eq(0).invoke('text').should('eq', ' fortestonly');
+      }); 
+});
+
+// Напишите тест на форму на отправку комментария
+// https://yevheniiahlovatska.editorx.io/web-practice/post/what-to-wear-to-a-blacktie-event
+
+describe ('Добавление комментария' , () => {
+   it('Авторизация сразу+удаление', () => {
+    cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/post/what-to-wear-to-a-blacktie-event').viewport('ipad-mini');
+    cy.wait(9000);
+    cy.get('[data-testid="section-container"] button').eq(0).click();
+    Cypress.on('uncaught:exception', (err) => { //ошибка Error: ResizeObserver loop limit exceeded
+    return false;
+     });
+    cy.get('[data-testid="signUp.switchToSignUp"]').click();
+    cy.get('[id*="Email"] button').click();
+    cy.get('[type="email"]').type('fortestonly@mail.com', {delay : 20});
+    cy.get('[type="password"]').type('Qwerty123*', {delay : 20});
+    cy.get('[data-testid="submit"] button').click();
+    cy.wait(5000);
+    cy.get('[data-hook="recent-posts"]').scrollIntoView();
+    cy.get('[data-hook="comment-box-placeholder"]').eq(0).click();
+    cy.get('[aria-describedby="placeholder-editor"]').type('comment1');
+    cy.get('[data-hook="submit-button"]').click();
+    cy.get('#viewer-foo', {timeout : 5000}).eq(0).should('have.text', 'comment1');
+    cy.wait(5000);
+    cy.get('[data-hook="actions-menu-btn"] svg').eq(0).scrollIntoView().click();
+    cy.get('[data-content="Delete"]').click();
+    cy.contains('[data-hook="submit-button"]', 'Delete').click();
+  });
+});
+
+describe ('Добавление комментария' , () => {
+ 
+  it('Авторизация после', () => {
+    cy.visit('https://yevheniiahlovatska.editorx.io/web-practice/post/what-to-wear-to-a-blacktie-event').viewport(768,1024);
+    cy.wait(9000);
+    cy.get('[data-hook="recent-posts"]').scrollIntoView();
+    cy.get('button[data-hook="comment-box-placeholder"]', {timeout : 10000}).eq(0).click();
+    Cypress.on('uncaught:exception', (err) => {  
+      return false;
+    });
+    cy.get('[aria-describedby="placeholder-editor"]').type('something');
+    cy.contains('Publish').click();
+    cy.get('[data-testid="signUp.switchToSignUp"]').click();
+    cy.wait(4000);
+    cy.get('[data-testid="buttonElement"]').click();
+    cy.get('input[type="email"]').type('fortestonly@mail.com', {delay : 20}); 
+    cy.get('input[type="password"]').type('Qwerty123*', {delay : 20});
+    cy.get('[data-testid="submit"] button').click();
+    cy.get('#viewer-foo', {timeout : 5000}).eq(0).should('have.text', 'something');
+  });
+});
---
+++ cypress/e2e/tasks/hometask9.cy.js
@@ -0,0 +1,52 @@
+/* Напишите тест на окно Авторизации https://www.beaxy.com/ */
+
+describe ('Авторизация' , () => {
+    before (() => {
+        cy.visit('https://www.beaxy.com/').viewport(1199,900);  /* кнопка login появляется в разрешении от 1199 */
+        cy.get('[class="modal-cookie-continue"]').click();
+    });
+    it('Login', () => {
+        cy.wait(5000); /* при неявном ожидании в строке клика на кнопку login тест выдавал ошибку, поэтому здесь и далее использовала явное (у меня сейчас общ. wifi в поездке, стабильность и скорость под вопросом) */
+        cy.get('[class*=header-buttons] [data-tomodal=login]').click();  
+        cy.get('#popUp-login-email').type('fortestonly@mail.com');
+        cy.get('#popUp-login-password').type('Qwerty123*');
+        cy.get('#login-checkbox').click();
+        cy.get('[class*=form-login] button').click();
+        cy.get('[data-at=info-container_name]', {timeout : 6000}).invoke('text').should('eq', 'fortestonly@mail.com');
+    });
+});
+
+/* Напишите тест на страницу careers https://www.beaxy.com/careers/ , где будут проверены все заголовки h1/h2/h3 на странице */
+
+describe ('Careers' , () => {   /* проверила что в верстке моб и деск версии приложения h1/h2/h3 те же самые, viewport не указывала */
+    before (() => {
+        cy.visit('https://www.beaxy.com/careers/');
+    });
+    it('Заголовки', () => { 
+        cy.get('[class*=header] h1').invoke('text').should('eq', ' Join a global team working on the future of finance');
+        cy.get('[class="body-container"] h2').invoke('text').should('eq', ' Why join Beaxy?');
+        cy.get('[class="footer-header"] h2').invoke('text').should('eq', ' Want to be part of our team?');
+        cy.xpath('//div[@class="body-content__item"][1]/h3').invoke('text').should('eq', ' Bridge gaps'); 
+        cy.xpath('//div[@class="body-content__item"][2]/h3').invoke('text').should('eq', ' Connect globally'); 
+        cy.xpath('//div[@class="body-content__item"][3]/h3').invoke('text').should('eq', ' Build powerfully'); 
+        cy.xpath('//div[@class="body-content__item"][4]/h3').invoke('text').should('eq', ' Value driven'); 
+        cy.xpath('//div[@class="body-content__item"][5]/h3').invoke('text').should('eq', ' Opportunities for everyone');
+        cy.xpath('//div[@class="body-content__item"][6]/h3').invoke('text').should('eq', ' Empowering freedom');  /* селекторы можно было короче/проще, но руководствовалась тем что по селектору видно h1/h2/h3 */
+    });
+});
+
+/* Напишите тест на страницу блог https://www.beaxy.com/blog/ , где необходимо проверить первый пост. (наличие картинки и названия поста) */
+
+describe ('Blog' , () => {   
+    before (() => {
+        cy.visit('https://www.beaxy.com/blog');
+    
+    });
+    it('Первый пост', () => {
+        cy.wait(8000); 
+        cy.get('button[class="show_all_pagi"]').click();   
+        cy.xpath('//div[@class="all_pagi active"]//a[@class="page-numbers"][last()]', {timeout : 8000}).click(); /* первый пост на последней странице */
+        cy.xpath('//div[@class="blog-item"][last()]//img').should('exist').should('be.visible'); 
+        cy.xpath('(//a[@class= "item-body__title"])[last()]').should('exist').should('be.visible');
+    });
+});
